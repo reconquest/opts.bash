88a1bb6fd166a7276441bdefcb7b24123e12207a
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2017-02-09 14:39:22 +0700

    use import.bash instead of submodule

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
-source ${0%/*}/vendor/github.com/reconquest/tests.sh/tests.sh
+source ${0%/*}/vendor/github.com/reconquest/import.bash/import.bash
+
+import:use github.com/reconquest/tests.sh
 
 tests:main -d tests/testcases -s tests/local-setup.sh "${@:--A}"
