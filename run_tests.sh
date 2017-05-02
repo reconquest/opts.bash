4defad1805070540a1532b0e5c625e68b91a3a70
Stanislav Seletskiy <s.seletskiy@gmail.com>
2017-05-02 23:24:14 +0300

    speedup tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 source ${0%/*}/vendor/github.com/reconquest/import.bash/import.bash
 
+# initialize vendors
+source opts.bash
+
 import:use github.com/reconquest/tests.sh
 
 tests:main -d tests/testcases -s tests/local-setup.sh "${@:--A}"
